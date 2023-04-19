8fb8051e9c15360423d25ddc7b61ca81a58364ad
Hardik29 <singhhardik14@gmail.com>
2023-04-19 17:37:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [quote, setQuote] = useState("")
+
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);  
+      }
+    )
+  }, [])
+
+
+  const handleClick = () => {
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);  
+      }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='quoteCard'>
+        <p>"{quote}"</p>
+        <button onClick={handleClick} className='quoteButton'>Get new Quote</button>
+      </div>
     </div>
   );
 }
